bdb02e4a0c871e304fe120e43adf746a2ec4b956
Sameer <21131a05g0@gvpce.ac.in>
2025-02-06 18:48:40 +0530

    Updated settings.py

--- file_converter/settings.py
+++ file_converter/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# file_converter/settings.py
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
