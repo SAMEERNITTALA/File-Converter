671d7deb09cb35b899ea8d034373d2a1e3f4921e
Sameer <21131a05g0@gvpce.ac.in>
2025-02-06 21:19:20 +0530

    Implemented ai-based identification

--- converter/views.py
+++ converter/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UploadFileForm
 from .models import UploadedFile
-from .ai_file_identification import identify_file_type
+from .ai_file_identification import identify_file_type, get_supported_conversions
 from .file_conversion import convert_file
 import os
 
@@ -15,9 +15,25 @@ def index(request):
             uploaded_file.file_type = identify_file_type(uploaded_file.file.path)
             uploaded_file.save()
 
+            # Get supported conversions for the file type
+            supported_conversions = get_supported_conversions(uploaded_file.file_type)
+            form = UploadFileForm(output_file_types=supported_conversions)
+            return render(request, 'converter/index.html', {'form': form, 'uploaded_file': uploaded_file})
+
+        # Handle conversion after selecting output file type
+        if 'convert' in request.POST:
+            uploaded_file = UploadedFile.objects.get(id=request.POST.get('file_id'))
+            uploaded_file.height = request.POST.get('height')
+            uploaded_file.width = request.POST.get('width')
+            uploaded_file.file_size = request.POST.get('file_size')
+            uploaded_file.output_file_name = request.POST.get('output_file_name')
+            uploaded_file.output_file_type = request.POST.get('output_file_type')
+            uploaded_file.save()
+
             # Perform file conversion
             converted_file_path = convert_file(
                 uploaded_file.file.path,
+                uploaded_file.output_file_type,
                 uploaded_file.height,
                 uploaded_file.width,
                 uploaded_file.file_size,
