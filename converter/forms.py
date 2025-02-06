cf22495ade3e770e8c45e33fa730f98be96e3b05
Sameer <21131a05g0@gvpce.ac.in>
2025-02-06 21:17:08 +0530

    Added a field output_file_type

--- converter/forms.py
+++ converter/forms.py
@@ -7,7 +7,13 @@ class UploadFileForm(forms.ModelForm):
     width = forms.IntegerField(required=False, label="Width (px)")
     file_size = forms.CharField(required=False, label="File Size (e.g., 5MB, 10KB)")
     output_file_name = forms.CharField(required=False, label="Output File Name")
+    output_file_type = forms.ChoiceField(choices=[], label="Output File Type")
 
     class Meta:
         model = UploadedFile
-        fields = ['file', 'height', 'width', 'file_size', 'output_file_name']
+        fields = ['file', 'height', 'width', 'file_size', 'output_file_name', 'output_file_type']
+
+    def __init__(self, *args, **kwargs):
+        output_file_types = kwargs.pop('output_file_types', [])
+        super().__init__(*args, **kwargs)
+        self.fields['output_file_type'].choices = output_file_types
